7e4e3d8dcef28a555fde47254566573a5c68a65e
João Silva <antnio.joao.joo393@gail.com>
2018-10-12 11:49:04 -0300

    Consertando um nos codastros que apareceu agora

--- cc-concluido.php
+++ cc-concluido.php
@@ -1,8 +1,9 @@
 <?php
 require_once("banco-usuario.php");
+require_once("logica-login.php");
 
-$cliente = buscaUsuarioCC($conexao, $_POST["email_mecanico"]);
-if($mecanico == null)
+$cliente = buscaUsuarioCC($conexao, $_POST["email_cliente"]);
+if($cliente == null)
 {
     $email_cliente = $_POST["email_cliente"];
     $senha_cliente = $_POST["senha_cliente"];
